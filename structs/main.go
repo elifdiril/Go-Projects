a7f9e7fdf48a5569ae72867c102033f2429074fd
Elif <elifdiril6@gmail.com>
2023-06-11 12:48:21 +0000

    structs project added.

---
+++ structs/main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
+	/*
+		var alex person
+
+		// { }
+		fmt.Println(alex)
+
+		//{firstName: lastName:}
+		fmt.Printf("%+v", alex)
+
+		alex.firstName = "Alex"
+		alex.lastName = "Anderson"
+
+		//{Alex Anderson}
+		fmt.Println(alex)
+
+		//{firstName:Alex lastName:Anderson}
+		fmt.Printf("%+v", alex)
+	*/
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 12345,
+		},
+	}
+
+	//{firstName:Jim lastName:Party contactInfo:{email:jim@gmail.com zipCode:12345}}
+	jim.print()
+
+	// pass by value not working
+	//jim.updateNameV1("jimmy")
+	//{firstName:Jim lastName:Party contactInfo:{email:jim@gmail.com zipCode:12345}}
+	//jim.print()
+
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+/*func (p person) updateNameV1(newFirstName string) {
+	p.firstName = newFirstName
+}*/
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
